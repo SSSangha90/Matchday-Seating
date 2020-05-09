237ce11979092761f21038d1f6085a9c3e6bb222
sukh <sukhveer.sangha@tatacommunications.com>
2020-05-09 15:28:06 +0100

    using local storage to set price, match and seat data

--- script.js
+++ script.js
@@ -9,11 +9,23 @@ const total = document.getElementById('total')
 const matchSelect = document.getElementById('match')
 
 let ticketPrice = +matchSelect.value 
-// plus(+) infront converts type to number
+// plus(+) infront converts datatype to number
+
+// Save selected match index and price
+const setMatchData = (matchIndex, matchPrice) => {
+    localStorage.setItem('selectedMatchIndex', matchIndex)
+    localStorage.setItem('selectedMatchPrice', matchPrice)
+}
 
 // update total and count
 const updateSelectedCount = () => {
     const selectedSeats = document.querySelectorAll('.row .seat.selected')
+
+    const seatsIndex = [...selectedSeats] // spread operator to copy elements of array above
+    .map((seat) => [...seats].indexOf(seat))
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
+
     const selectedSeatsCount = selectedSeats.length
     count.innerText = selectedSeatsCount
     total.innerText = selectedSeatsCount * ticketPrice
@@ -22,6 +34,7 @@ const updateSelectedCount = () => {
 // Match select event
 matchSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value
+    setMatchData(e.target.selectedIndex, e.target.value);
     updateSelectedCount()
 })
 
